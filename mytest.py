b5df6c1f4870996c7268c4a30e015c8e6f4538f5
wh0001992 <728304939@qq.com>
2020-02-29 18:45:31 +0800

    python基础上课练习第一天

---
+++ mytest.py
@@ -0,0 +1,48 @@
+#a = 2
+'''
+b = 2.5
+c = 'happy'
+print(a,b,c)
+'''
+
+'''
+a = 8
+b = 9.8
+c = 'my world'
+d = input('请输入你想要输入的信息：')
+#print(('%d,%f,%s')%(a,b,c))
+#print('他是最小的={}，他比较大={}'.format(a,b))
+print(a,b,type(a),type(b),type(d))
+'''
+
+#s = (1,2,3,4,5,6,7,8,9)
+#rint(s[-3])
+#print(s[3:8:2])
+'''
+s = [5,2,3,9,1,6,7,8,1,4]
+a = [99,88]
+s.insert(4,10)
+s.append(11)
+s.extend(a)
+c = s.pop(2)
+#s.remove(1)
+d = s.count(1)
+e = max(s)
+f = min(s)
+g = len(s)
+del s[0]
+h = s.index(4)
+#s.clear()
+print(s)
+print(h)
+'''
+'''
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+'''
+a = 2
+a = a*6
+print(a)
